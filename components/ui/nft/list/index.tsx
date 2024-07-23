f71622c7014c24d77f3a98b9f772d016d1570055
Edwin-Farid <117edwin@gmail.com>
2024-07-24 07:48:08 +0800

    fix: nft card

--- components/ui/nft/list/index.tsx
+++ components/ui/nft/list/index.tsx
@@ -10,7 +10,7 @@ const NftList: FunctionComponent= () => {
   const {nfts} = useListedNfts();
   
     return (
-    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-4 lg:max-w-none">
       { nfts.data?.map(nft =>
         <div key={nft.meta.image} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
           <NftItem 
